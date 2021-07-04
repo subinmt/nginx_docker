d888936f9914e6365e82ac9a66914a98e0ef9417
Subin M Thevalakkattu <37261883+subinmt@users.noreply.github.com>
2021-07-04 12:18:37 +0530

    role updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,11 +15,41 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-09e67e426f25ce0d7"
+  ami           = "ami-050dd51d996d9a69e"
   instance_type = "t3.medium"
-  key_name = "sm-virgina"
+  key_name = "sm-virginia"
+  iam_instance_profile = aws_iam_instance_profile.jenkins_profile.name
 
   tags = {
     Name = "AppServerInstance"
   }
+}
+
+resource "aws_iam_role" "jenkins_role" {
+  name = "jenkins-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryFullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+  role       = aws_iam_role.jenkins_role.name
+}
+
+resource "aws_iam_instance_profile" "jenkins_profile" {
+  name  = "jenkins_profile"
+  role = aws_iam_role.jenkins_role.name
 }
